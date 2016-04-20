8ff66ce296b782f5b3e10e71f8657fe76f50d79b
XeoXeo <ced2595@hotmail.com>
2016-04-20 14:36:11 +0000

    Modified css and added javascript file

---
+++ strip_manager/static/js/script.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded',
+    function ()
+    {
+        // Process needed code here
+        // Ask for number of LEDs
+        // -> Generate grid from the amount of LEDs
+        // -> Retreive LED information if any
+
+        // Add eventhandler when clicking on an object
+        // -> Show a color picker
+        // -> Color chosen = click the color area to save it
+    },
+    false
+);
+
+function getLEDCount() {
+    return count;
+}
